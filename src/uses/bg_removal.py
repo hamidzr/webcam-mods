601c934e26d1e6263e2f716f23fa19d50254e4a7
Hamid Zare <hamidzare.vu@gmail.com>
2021-07-22 00:12:02 -0700

    limit importing only the crop out of uses/crop

--- src/uses/bg_removal.py
+++ src/uses/bg_removal.py
@@ -1,6 +1,6 @@
 from loopback import live_loop, OUT_WIDTH, OUT_HEIGHT
 from mods.person_segmentation import color_bg, blur_bg, swap_bg
-from uses.crop_cam import frame_modr as crop_modr
+from uses.crop_cam import shmem, crop
 import os.path
 from pathlib import Path
 import cv2
@@ -8,9 +8,8 @@ import cv2
 bg_image = cv2.resize(cv2.imread(f'{Path.cwd()}/data/bg.jpg'), (OUT_WIDTH, OUT_HEIGHT))
 
 def frame_modr(frame):
-    # frame = mask(frame, OUT_WIDTH, OUT_HEIGHT)
     # TODO this should be more modular
-    frame = crop_modr(frame)
+    frame = crop(frame, OUT_WIDTH, OUT_HEIGHT, shmem[0], shmem[1])
     frame = swap_bg(frame, bg_image)
     return frame
 
