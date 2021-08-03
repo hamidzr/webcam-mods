cad5f8d3b9ade446dde5eb1577368b19cc3bc9ae
Hamid Zare <hamidzare.vu@gmail.com>
2021-08-03 03:05:34 -0700

    finish setting interactive control boundaries

--- src/uses/crop_cam.py
+++ src/uses/crop_cam.py
@@ -53,7 +53,7 @@ def on_press(key):
     if not any(key in cur_keys for key in target_keys):
         return
     if (Key.ctrl in cur_keys):
-        if (Key.shift in cur_keys):
+        if (Key.shift in cur_keys): # control crop dimensions
             if key == Key.right:
                 cf.crop_dims[0] = min(IN_WIDTH, cf.crop_dims[0] + JUMP)
             elif key == Key.left:
@@ -62,7 +62,7 @@ def on_press(key):
                 cf.crop_dims[1] = min(IN_HEIGHT, cf.crop_dims[1] + JUMP)
             elif key == Key.down:
                 cf.crop_dims[1] = max(0, cf.crop_dims[1] - JUMP)
-        else:
+        else: # control crop position
             # left and right are reversed to compensate for mirror effects
             if key == Key.right:
                 cf.crop_pos[0] = max(0, cf.crop_pos[0] - JUMP)
@@ -72,21 +72,18 @@ def on_press(key):
                 cf.crop_pos[1] = max(0, cf.crop_pos[1] - JUMP)
             elif key == Key.down:
                 cf.crop_pos[1] = min(IN_HEIGHT, cf.crop_pos[1] + JUMP)
-    if (Key.alt in cur_keys):
+    if (Key.alt in cur_keys): # control frame padding
         if key == Key.right:
             cf.pad_size[0] = max(0, cf.pad_size[0] - JUMP)
         elif key == Key.left:
-            cf.pad_size[0] += JUMP
+            cf.pad_size[0] = min(IN_WIDTH, cf.pad_size[0] + JUMP)
         elif key == Key.up:
             cf.pad_size[1] = max(0, cf.pad_size[1] - JUMP)
         elif key == Key.down:
-            cf.pad_size[1] += JUMP
+            cf.pad_size[1] = min(IN_HEIGHT, cf.pad_size[1] + JUMP)
 
-    # val_max = max(OUT_WIDTH, OUT_HEIGHT)
-    # shmem = [min(max(x,0), val_max) for x in shmem] # limit the values albeit poorly
-    # with open(CONF_FILE, 'w+') as f:
-    #     f.write(','.join([str(x) for x in shmem]))
     cf.persist() # TODO reduce unnecessary writes
+    print(cf)
 
 
 def on_release(key):
