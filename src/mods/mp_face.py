5c39fca39d20a85ec3e18f3fc909d87993f8ccd4
Hamid Zare <hamidzare.vu@gmail.com>
2021-10-25 23:02:00 -0700

    set shiftwidth 4 indent

--- src/mods/mp_face.py
+++ src/mods/mp_face.py
@@ -1,36 +1,50 @@
+from src.geometry import Rect
 import cv2
 from mediapipe.python.solutions import face_detection
 from typing import Optional, Tuple
+from src.utils.video import sleep_until_fps
 
 
 fd: Optional[face_detection.FaceDetection] = None
 # fd.close()
 def init() -> face_detection.FaceDetection:
-  global fd
-  if fd is None:
-    fd = face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.6)
-  return fd
+    global fd
+    if fd is None:
+      fd = face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.6)
+    return fd
 
 def predict(frame) -> Optional[Tuple[int, int, int, int]]:
-  """
-  predicts a bounding box around the first face found:
-  xmin: 0.43449878692626953
-  ymin: 0.47915118932724
-  width: 0.1905471682548523
-  height: 0.2540317177772522
-  """
-  image = frame
-  fh, fw, _ = frame.shape
-  fd = init()
-    # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
-  results = fd.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-  if not results.detections:
-    return None
-  detection = results.detections[0] # target detection supporting single face usage.
-  # print('Nose tip:')
-  # print(face_detection.get_key_point(
-  #     detection, face_detection.FaceKeyPoint.NOSE_TIP))
-  return detection.location_data.relative_bounding_box
+    """
+    predicts a bounding box around the first face found:
+    xmin: 0.43449878692626953
+    ymin: 0.47915118932724
+    width: 0.1905471682548523
+    height: 0.2540317177772522
+    """
+    image = frame
+    fh, fw, _ = frame.shape
+    fd = init()
+      # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
+    results = fd.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    if not results.detections:
+      return None
+    detection = results.detections[0] # target detection supporting single face usage.
+    # print('Nose tip:')
+    # print(face_detection.get_key_point(
+    #     detection, face_detection.FaceKeyPoint.NOSE_TIP))
+    return detection.location_data.relative_bounding_box
+
+# def abs_boundingbox(frame, relbb) -> Rect:
+
+
+if __name__ == '__main__':
+    from src.input.video_dev import Webcam
+    init()
+    with Webcam() as (cam, _):
+        while True:
+            pred = predict(cam.frame())
+            print(pred)
+            sleep_until_fps(5)
 
 # from src.loopback import OUT_WIDTH, OUT_HEIGHT
 # from src.mods.video_mods import crop
