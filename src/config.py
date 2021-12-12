39db969ee3f4f4ab23d3e4e14ad160c377869570
Hamid Zare <hamidzare.vu@gmail.com>
2021-12-12 14:19:42 -0800

    double the default output dimensions.
    make out_width and height configurable

--- src/config.py
+++ src/config.py
@@ -6,10 +6,10 @@ IN_WIDTH = int(os.getenv('IN_WIDTH', 640))
 IN_HEIGHT = int(os.getenv('IN_HEIGHT', 480))
 
 VIDEO_OUT = 10
-OUT_WIDTH = 320  # 240
-OUT_HEIGHT = 240  # 320
-# OUT_WIDTH = 640
-# OUT_HEIGHT = 480
+OUT_WIDTH = int(os.getenv('OUT_WIDTH', 640))
+OUT_HEIGHT = int(os.getenv('OUT_HEIGHT', 480))
+
+# adjust if perfromance is an issue in some of the process heavy mods
 MAX_OUT_FPS = int(os.getenv('MAX_OUT_FPS', 30))
 
 NO_SIGNAL_IMAGE = cv2.imread(f'{Path.cwd()}/data/nosignal.jpg')
